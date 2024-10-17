1cd42e001983055b06a0c571621d454b6df29c36
AssassinZ <azpranto11@gmail.com>
2024-10-17 21:56:53 +0600

    This is a test commit

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> vec;
+
+    vec.push_back(1);
+    vec.push_back(9);
+    vec.push_back(6);
+
+    cout << vec.size() << endl;
+    cout << vec.capacity() << endl;
+    
+    return 0;
+}
