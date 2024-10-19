719f5bdf2b47af12d62568bc88fb0391589bffef
AssassinZ <azpranto11@gmail.com>
2024-10-19 17:46:56 +0600

    Brute force, not for LC

--- pairSumBF.cpp
+++ pairSumBF.cpp
@@ -12,8 +12,8 @@ int main() {
     for(int i = 0; i < sz; i++) {
         for(int j = i+1; j < sz; j++) {
             if (vec[i] + vec[j] == target) {
-                ans.push_back(vec[i]);
-                ans.push_back(vec[j]);
+                ans.push_back(i);      //adds the index of vec
+                ans.push_back(j);      //same
                 cout << ans[0] << ", " << ans[1] <<endl;
                 break;
             }
