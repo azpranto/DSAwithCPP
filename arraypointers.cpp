6ad2c34e973e74a07c9153962548244b8767cd9e
AssassinZ <azpranto11@gmail.com>
2024-12-12 21:47:04 +0600

    theory

--- arraypointers.cpp
+++ arraypointers.cpp
@@ -9,5 +9,7 @@ int main() {
     cout << "address of arr: " << arr << endl; // array itself is a pointer which has a location and it always points 0th index element. the address is basically the address of the 0th value location
     cout << "value of 0th index of arr: " << *arr << endl;
 
+    // array is a constant pointer of which value can never be changed
+    
     return 0;
 }
